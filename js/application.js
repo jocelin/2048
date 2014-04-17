7118d1b6f9975edc0d71335befd2e42b4179357e
Jocelyn Lin <jocelin.cs@gmail.com>
2014-04-16 23:59:29 -0700

    update index info and layout

--- js/application.js
+++ js/application.js
@@ -2,8 +2,6 @@
 
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-
-  updateTileBackgrounds();
   new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
 });
 
